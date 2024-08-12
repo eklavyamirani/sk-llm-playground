9d6a105f1170b11a81bc54d0205334c5ae184142
Eklavya Mirani <eklavyamirani@hotmail.com>
2024-08-12 03:21:26 +0000

    initial repo setup

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+# needs privilege, so ignore for now.
+# dotnet workload update
+cd sk-llm-playground-console
+echo "init dotnet user secrets"
+dotnet user-secrets init
+
+if [ -z "$OPENAI_APIKEY"]
+then
+    echo "API KEY NOT SET"
+    exit 2
+fi
+
+echo "open ai key debug $OPENAI_APIKEY"
+dotnet user-secrets set "OpenAI:apiKey" $OPENAI_APIKEY
---
+++ sk-llm-playground-console/Program.cs
@@ -0,0 +1,57 @@
+﻿// Import packages
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+// FIXME: @eklavya fill the config below
+var modelId ="";
+var apiKey = "";
+
+// Create a kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion(modelId: modelId, apiKey: apiKey);
+
+// TODO: @eklavya add logging
+////builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+// Build the kernel
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    Temperature = 0, 
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do {
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(userInput))
+    {
+        Console.WriteLine("No input. The end.");
+        break;
+    }
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? "No content");
+} while (true);
